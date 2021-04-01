8e52748be659574e0a4ab154d33ba4c37e740997
Nick J Lyon <nick.lyon@Nicks-MacBook-Pro.local>
2021-04-01 12:30:31 -0400

    Upon acceptance we got some (minor) editing that needed to be done to our map figure (e.g., size of inset, north arrow, etc.)

--- Code/site_map.R
+++ Code/site_map.R
@@ -4,14 +4,14 @@
 # Written by Nicholas Lyon
 
 # PURPOSE #### 
-  ## Everybody likes a nice clean site map, so that's what this script will give 'em
+  ## Everybody likes a nice clean site map, so that's what this script will give them
 
 # Necessary libraries
-library(rgdal); library(sp); library(raster)
-                
+library(rgdal); library(sp); library(raster); library(maps); library(GISTools)  
+
 # Set the working directory
-setwd("~/Documents/_Publications/2020_Lyon_Butterfly SnS/Herbicide.WD")
-                
+setwd("~/Documents/_Publications/2021_Lyon_Butterfly SnS/Herbicide.WD")
+
 # Clear the environment
 rm(list = ls())
 
@@ -53,7 +53,7 @@ par(.pardefault)
 
 # Make the plot!
   ## State borders
-plot(region.states, col = "gray99", xlim = c(-94.16, -94.14), ylim = c(40.545, 40.608))
+plot(region.states, col = "gray99", xlim = c(-94.16, -94.14), ylim = c(40.549, 40.611))
   ## Make a black box around the map
 box(which = "plot", lty = "solid")
   ## Add the three sites' maps
@@ -67,7 +67,9 @@ text("PYW", cex = 0.75, x = -94.176, y = 40.5825)
 text("IA", font = 3, cex = 1, x = -94.115, y = 40.5755)
 text("MO", font = 3, cex = 1, x = -94.115, y = 40.569)
   ## Add scale bar
-map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56)
+maps::map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56, cex = 0.85)
+  ## Add a north arrow
+north.arrow(xb = -94.11, yb = 40.555, len = 0.002, lab = "N", col = 'gray10') 
   ## Add lat/long axes
 map.axes(cex.axis = 0.8)
 
@@ -82,7 +84,9 @@ plot(grg.geo, add = T, col = "gray75")
 text("GRG", font = 3, cex = 0.7, x = -94.1, y = 41.02)
 text("IA", cex = 1.4, x = -93.5, y = 42.1)
 text("MO", cex = 1.4, x = -92.75, y = 38.5)
-text("IL", cex = 1.4, x = -89.4, y = 40.2)
+text("IL", cex = 1.4, x = -90.1, y = 40.2)
+text("NE", cex = 1.4, x = -96.8, y = 41)
+text("KS", cex = 1.4, x = -96.5, y = 39)
 
 # Now make both plots and save them separately
   ## Site Map
@@ -97,7 +101,8 @@ text("LTR", cex = 0.75, x = -94.14, y = 40.5825)
 text("PYW", cex = 0.75, x = -94.176, y = 40.5825)
 text("IA", font = 3, cex = 1, x = -94.105, y = 40.5755)
 text("MO", font = 3, cex = 1, x = -94.105, y = 40.569)
-map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56)
+maps::map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56, cex = 0.8)
+north.arrow(xb = -94.11, yb = 40.555, len = 0.002, lab = "N", col = 'gray10') 
 map.axes(cex.axis = 0.8)
 dev.off()
 
@@ -109,7 +114,9 @@ plot(grg.geo, add = T, col = "gray75")
 text("GRG", font = 3, cex = 0.7, x = -94.1, y = 41.02)
 text("IA", cex = 1.4, x = -93.5, y = 42.1)
 text("MO", cex = 1.4, x = -92.75, y = 38.5)
-text("IL", cex = 1.4, x = -89.4, y = 40.2)
+text("IL", cex = 1.4, x = -90.1, y = 40.2)
+text("NE", cex = 1.4, x = -96.8, y = 41)
+text("KS", cex = 1.4, x = -96.5, y = 39)
 dev.off()
 
 ## -------------------------------------------- ##
@@ -120,24 +127,26 @@ dev.off()
 # Re-set the parameter to default (if changed downstream)
 par(.pardefault)
 
-# Make the plot!
-## State borders
-plot(region.states, col = "gray99", xlim = c(-94.16, -94.14), ylim = c(40.545, 40.608))
-## Make a black box around the map
+# Begin with the site map
+  ## State borders
+plot(region.states, col = "gray99", xlim = c(-94.16, -94.14), ylim = c(40.549, 40.611))
+  ## Make a black box around the map
 box(which = "plot", lty = "solid")
-## Add the three sites' maps
+  ## Add the three sites' maps
 plot(gil.geo, add = T, col = "#4575b4")
 plot(ltr.geo, add = T, col = "#d73027")
 plot(pyw.geo, add = T, col = "#fee090")
-## Add text for the sites and states
+  ## Add text for the sites and states
 text("GIL", cex = 0.75, x = -94.1255, y = 40.59)
 text("LTR", cex = 0.75, x = -94.14, y = 40.5825)
 text("PYW", cex = 0.75, x = -94.176, y = 40.5825)
 text("IA", font = 3, cex = 1, x = -94.115, y = 40.5755)
 text("MO", font = 3, cex = 1, x = -94.115, y = 40.569)
-## Add scale bar
-map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56)
-## Add lat/long axes
+  ## Add scale bar
+maps::map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56, cex = 0.75)
+  ## Add a north arrow
+north.arrow(xb = -94.11, yb = 40.6, len = 0.002, lab = "N", col = 'gray10') 
+  ## Add lat/long axes
 map.axes(cex.axis = 0.8)
 
 # Create inset dimensions
@@ -146,22 +155,33 @@ par(new = T)
   ## Eliminate margins
 par(mar = c(0, 0, 0, 0))
   ## Set the size of the box (xmin, xmax, ymin, ymax)
-par(fig = c(0.26, 0.48, 0.7, 0.85))
+par(fig = c(0.26, 0.66, 0.6, 0.9))
 
 # Make another plot of the larger state region
-## Plot the larger region
+  ## Plot the larger region
 plot(region.states, col = "gray99", xlim = c(-93.555, -93.55), ylim = c(38, 43.5))
-## Make a black box around the map
+  ## Make a black box around the map
 box(which = "plot", lty = "solid")
-## Add the outline of the larger GRG region
+  ## Add the outline of the larger GRG region
 plot(grg.geo, add = T, col = "gray75")
-## Add text to define some key bits
-text("GRG", font = 3, cex = 0.4, x = -94.1, y = 41.4)
+  ## Add a scale bar in the inset
+maps::map.scale(ratio = F, relwidth = 0.2, x = -94.1, y = 39, cex = 0.4)
+  ## Add text to define some key bits
+text("GRG", font = 3, cex = 0.4, x = -94.1, y = 41.2)
+text("IA", cex = 0.8, x = -93.5, y = 42.1)
+text("MO", cex = 0.8, x = -92.75, y = 40)
+text("IL", cex = 0.8, x = -90.1, y = 40.2)
+text("NE", cex = 0.8, x = -96.8, y = 41)
+text("KS", cex = 0.8, x = -96.5, y = 39)
 
+## -------------------------------------------- ##
+          # Fancy Inset Map Saving ####
+## -------------------------------------------- ##
 # Looks great! Now re-run it to save it
+  ## Below is copy/pasted from above so if you need comments see above
 jpeg(filename = "./Figures/Figure 1.jpeg", width = 6, height = 6, units = "in", res = 720)
 par(.pardefault)
-plot(region.states, col = "gray99", xlim = c(-94.16, -94.14), ylim = c(40.545, 40.608))
+plot(region.states, col = "gray99", xlim = c(-94.16, -94.14), ylim = c(40.549, 40.611))
 box(which = "plot", lty = "solid")
 plot(gil.geo, add = T, col = "#4575b4")
 plot(ltr.geo, add = T, col = "#d73027")
@@ -171,16 +191,24 @@ text("LTR", cex = 0.75, x = -94.14, y = 40.5825)
 text("PYW", cex = 0.75, x = -94.176, y = 40.5825)
 text("IA", font = 3, cex = 1, x = -94.115, y = 40.5755)
 text("MO", font = 3, cex = 1, x = -94.115, y = 40.569)
-map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56)
+maps::map.scale(ratio = F, relwidth = 0.2, x = -94.19, y = 40.56, cex = 0.75)
+north.arrow(xb = -94.11, yb = 40.6, len = 0.002, lab = "N", col = 'gray10') 
 map.axes(cex.axis = 0.8)
 par(new = T)
 par(mar = c(0, 0, 0, 0))
-par(fig = c(0.26, 0.48, 0.7, 0.85))
+par(fig = c(0.26, 0.66, 0.6, 0.9))
 plot(region.states, col = "gray99", xlim = c(-93.555, -93.55), ylim = c(38, 43.5))
 box(which = "plot", lty = "solid")
 plot(grg.geo, add = T, col = "gray75")
-text("GRG", font = 3, cex = 0.4, x = -94.1, y = 41.4)
+maps::map.scale(ratio = F, relwidth = 0.2, x = -94.1, y = 39, cex = 0.4)
+text("GRG", font = 3, cex = 0.4, x = -94.1, y = 41.2)
+text("IA", cex = 0.8, x = -93.5, y = 42.1)
+text("MO", cex = 0.8, x = -92.75, y = 40)
+text("IL", cex = 0.8, x = -90.1, y = 40.2)
+text("NE", cex = 0.8, x = -96.8, y = 41)
+text("KS", cex = 0.8, x = -96.5, y = 39)
 dev.off()
+
 
 # END ####
 
