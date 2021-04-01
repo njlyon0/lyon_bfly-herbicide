2cba15be18a0743ceb5dfe41abf8964912f31ece
Nick J Lyon <nick.lyon@Nicks-MacBook-Pro.local>
2021-04-01 12:30:06 -0400

    Upon acceptance we got some (minor) editing that needed to be done to our figures (border around plot area, panel labeling, etc.)

--- Code/figures.R
+++ Code/figures.R
@@ -55,6 +55,7 @@ pref.theme <- theme(panel.grid.major = element_blank(), panel.grid.minor = eleme
                     legend.title = element_blank(), legend.position = "none")
 no.y.axis <- theme(axis.title.y = element_blank(), axis.text.y = element_blank(),
                    axis.ticks.y = element_blank(), axis.line.y = element_blank())
+pref.panel.bord <- panel_border(color = "black", size = 0.75)
 
 ##  ----------------------------------------------------------  ##
                      # Butterfly Figure ####
@@ -72,8 +73,9 @@ bf.14.abun.plt <- ggplot(bf.14.abun.pltdf, aes(x = Herb.Trt, y = Abundance)) +
   scale_color_manual(values = bf.colors) +
   scale_fill_manual(values = bf.colors) +
   ylim(0, 135) +
-  labs(x = "Pre-Treatment", y = "Butterfly Abundance") +
-  pref.theme; bf.14.abun.plt
+  labs(x = "Pre-treatment", y = "Butterfly abundance") +
+  pref.theme + theme(axis.title.x = element_blank()) +
+  pref.panel.bord; bf.14.abun.plt
 
   ## Post-Treatment (2015-18)
 bf.abun.plt <- ggplot(bf.abun.pltdf, aes(x = Year, y = Abundance)) +
@@ -82,12 +84,13 @@ bf.abun.plt <- ggplot(bf.abun.pltdf, aes(x = Year, y = Abundance)) +
   geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 135) +
   scale_fill_manual(values = bf.colors) +
   scale_color_manual(values = bf.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + theme(legend.position = c(0.8, 0.9)) + no.y.axis; bf.abun.plt
+  pref.theme + theme(legend.position = c(0.85, 0.75), axis.title.x = element_blank()) +
+  no.y.axis + pref.panel.bord; bf.abun.plt
 
   ## Make a two-panel figure
 bf.abun.fig <- egg::ggarrange(bf.14.abun.plt, bf.abun.plt, nrow = 1, widths = c(1, 2.5))
@@ -105,8 +108,8 @@ bf.14.rich.plt <- ggplot(bf.14.rich.pltdf, aes(x = Herb.Trt, y = Richness)) +
   scale_color_manual(values = bf.colors) +
   scale_fill_manual(values = bf.colors) +
   ylim(0, 16) +
-  labs(x = "Pre-Treatment", y = "Butterfly Richness") +
-  pref.theme; bf.14.rich.plt
+  labs(x = "Pre-treatment", y = "Butterfly richness") +
+  pref.theme + theme(axis.title.x = element_blank()) + pref.panel.bord; bf.14.rich.plt
 
   ## Post-Treatment (2015-18)
 bf.rich.plt <- ggplot(bf.rich.pltdf, aes(x = Year, y = Richness)) +
@@ -115,12 +118,12 @@ bf.rich.plt <- ggplot(bf.rich.pltdf, aes(x = Year, y = Richness)) +
   geom_errorbar(aes(ymax = Richness + se, ymin = Richness - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 16) +
   scale_fill_manual(values = bf.colors) +
   scale_color_manual(values = bf.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + no.y.axis; bf.rich.plt
+  pref.theme + theme(axis.title.x = element_blank()) + no.y.axis + pref.panel.bord; bf.rich.plt
 
   ## Make a two-panel figure
 bf.rich.fig <- egg::ggarrange(bf.14.rich.plt, bf.rich.plt, nrow = 1, widths = c(1, 2.5))
@@ -138,8 +141,8 @@ bf.14.dive.plt <- ggplot(bf.14.dive.pltdf, aes(x = Herb.Trt, y = Diversity)) +
   scale_color_manual(values = bf.colors) +
   scale_fill_manual(values = bf.colors) +
   ylim(0, 2.75) +
-  labs(x = "Pre-Treatment", y = "Butterfly Diversity") +
-  pref.theme; bf.14.dive.plt
+  labs(x = "Pre-treatment", y = "Butterfly diversity") +
+  pref.theme + pref.panel.bord; bf.14.dive.plt
 
   ## Post-Treatment (2015-18)
 bf.dive.plt <- ggplot(bf.dive.pltdf, aes(x = Year, y = Diversity)) +
@@ -148,18 +151,19 @@ bf.dive.plt <- ggplot(bf.dive.pltdf, aes(x = Year, y = Diversity)) +
   geom_errorbar(aes(ymax = Diversity + se, ymin = Diversity - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 2.75) +
   scale_fill_manual(values = bf.colors) +
   scale_color_manual(values = bf.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + no.y.axis; bf.dive.plt
+  pref.theme + no.y.axis + pref.panel.bord; bf.dive.plt
 
   ## Make a two-panel figure
 bf.dive.fig <- egg::ggarrange(bf.14.dive.plt, bf.dive.plt, nrow = 1, widths = c(1, 2.5))
 
 # Make three-panel butterfly figure
-plot_grid(bf.abun.fig, bf.rich.fig, bf.dive.fig, labels = c("i", "ii", "iii"), ncol = 1, nrow = 3)
+plot_grid(bf.abun.fig, bf.rich.fig, bf.dive.fig, labels = c("A", "B", "C"),
+          align = 'v', ncol = 1, nrow = 3)
 
 # Save it
 ggsave(plot = last_plot(), filename = "./Figures/Figure 3.pdf", width = 7, height = 9, units = "in")
@@ -180,8 +184,8 @@ flr.14.abun.plt <- ggplot(flr.14.abun.pltdf, aes(x = Herb.Trt, y = Abundance)) +
   scale_color_manual(values = flr.colors) +
   scale_fill_manual(values = flr.colors) +
   ylim(0, 7000) +
-  labs(x = "Pre-Treatment", y = "Floral Abundance") +
-  pref.theme; flr.14.abun.plt
+  labs(x = "Pre-treatment", y = "Floral abundance") +
+  pref.theme + theme(axis.title.x = element_blank()) + pref.panel.bord; flr.14.abun.plt
 
   ## Post-Treatment (2015-18)
 flr.abun.plt <- ggplot(flr.abun.pltdf, aes(x = Year, y = Abundance)) +
@@ -189,12 +193,13 @@ flr.abun.plt <- ggplot(flr.abun.pltdf, aes(x = Year, y = Abundance)) +
   geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 7000) +
   scale_fill_manual(values = flr.colors) +
   scale_color_manual(values = flr.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + theme(legend.position = c(0.8, 0.9)) + no.y.axis; flr.abun.plt
+  pref.theme + theme(legend.position = c(0.8, 0.7), axis.title.x = element_blank()) +
+  no.y.axis + pref.panel.bord; flr.abun.plt
 
   ## Make a two-panel figure
 flr.abun.fig <- egg::ggarrange(flr.14.abun.plt, flr.abun.plt, nrow = 1, widths = c(1, 2.5))
@@ -212,8 +217,8 @@ flr.14.rich.plt <- ggplot(flr.14.rich.pltdf, aes(x = Herb.Trt, y = Richness)) +
   scale_color_manual(values = flr.colors) +
   scale_fill_manual(values = flr.colors) +
   ylim(0, 25) +
-  labs(x = "Pre-Treatment", y = "Floral Richness") +
-  pref.theme; flr.14.rich.plt
+  labs(x = "Pre-treatment", y = "Floral richness") +
+  pref.theme + theme(axis.title.x = element_blank()) + pref.panel.bord; flr.14.rich.plt
 
   ## Post-Treatment (2015-18)
 flr.rich.plt <- ggplot(flr.rich.pltdf, aes(x = Year, y = Richness)) +
@@ -222,7 +227,7 @@ flr.rich.plt <- ggplot(flr.rich.pltdf, aes(x = Year, y = Richness)) +
   geom_errorbar(aes(ymax = Richness + se, ymin = Richness - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 25) +
   geom_text(label = "A", x = 15.5, y = 12) +
   geom_text(label = "AB", x = 14.7, y = 14.5) +
@@ -230,7 +235,7 @@ flr.rich.plt <- ggplot(flr.rich.pltdf, aes(x = Year, y = Richness)) +
   scale_fill_manual(values = flr.colors) +
   scale_color_manual(values = flr.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + no.y.axis; flr.rich.plt
+  pref.theme + theme(axis.title.x = element_blank()) + no.y.axis + pref.panel.bord; flr.rich.plt
 
   ## Make a two-panel figure
 flr.rich.fig <- egg::ggarrange(flr.14.rich.plt, flr.rich.plt, nrow = 1, widths = c(1, 2.5))
@@ -248,8 +253,8 @@ flr.14.dive.plt <- ggplot(flr.14.dive.pltdf, aes(x = Herb.Trt, y = Diversity)) +
   scale_color_manual(values = flr.colors) +
   scale_fill_manual(values = flr.colors) +
   ylim(0, 2.75) +
-  labs(x = "Pre-Treatment", y = "Floral Diversity") +
-  pref.theme; flr.14.dive.plt
+  labs(x = "Pre-treatment", y = "Floral diversity") +
+  pref.theme + pref.panel.bord; flr.14.dive.plt
 
 ## Post-Treatment (2015-18)
 flr.dive.plt <- ggplot(flr.dive.pltdf, aes(x = Year, y = Diversity)) +
@@ -258,7 +263,7 @@ flr.dive.plt <- ggplot(flr.dive.pltdf, aes(x = Year, y = Diversity)) +
   geom_smooth(method = "lm", se = F, color = "black") +
   #geom_smooth(aes(color = Herb.Trt), method = "lm", se = F, linetype = 4) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 2.75) +
   geom_text(label = "AB", x = 15.5, y = 1.1) + # Spr
   geom_text(label = "A", x = 14.7, y = 1.35) + # Con
@@ -266,13 +271,14 @@ flr.dive.plt <- ggplot(flr.dive.pltdf, aes(x = Year, y = Diversity)) +
   scale_fill_manual(values = flr.colors) +
   scale_color_manual(values = flr.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + no.y.axis; flr.dive.plt
+  pref.theme + no.y.axis + pref.panel.bord; flr.dive.plt
 
   ## Make a two-panel figure
 flr.dive.fig <- egg::ggarrange(flr.14.dive.plt, flr.dive.plt, nrow = 1, widths = c(1, 2.5))
 
 # Make three-panel butterfly figure
-plot_grid(flr.abun.fig, flr.rich.fig, flr.dive.fig, labels = c("i", "ii", "iii"), ncol = 1, nrow = 3)
+plot_grid(flr.abun.fig, flr.rich.fig, flr.dive.fig, labels = c("A", "B", "C"),
+          align = 'v', ncol = 1, nrow = 3)
 
 # Save it
 ggsave(plot = last_plot(), filename = "./Figures/Figure 2.pdf", width = 7, height = 9, units = "in")
@@ -320,8 +326,8 @@ sdmx.14.abun.plt <- ggplot(sdmx.14.abun.pltdf, aes(x = Herb.Trt, y = Abundance))
   scale_color_manual(values = sdmx.colors) +
   scale_fill_manual(values = sdmx.colors) +
   ylim(0, 1500) +
-  labs(x = "Pre-Treatment", y = "Seeded Species Abundance") +
-  pref.theme; sdmx.14.abun.plt
+  labs(x = "Pre-treatment", y = "Seeded species abundance") +
+  pref.theme + theme(axis.title.x = element_blank()) + pref.panel.bord; sdmx.14.abun.plt
 
   ## Post-Treatment (2015-18)
 sdmx.abun.plt <- ggplot(sdmx.abun.pltdf, aes(x = Year, y = Abundance)) +
@@ -329,12 +335,13 @@ sdmx.abun.plt <- ggplot(sdmx.abun.pltdf, aes(x = Year, y = Abundance)) +
   geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 1500) +
   scale_fill_manual(values = sdmx.colors) +
   scale_color_manual(values = sdmx.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + theme(legend.position = c(0.85, 0.95))+ no.y.axis; sdmx.abun.plt
+  pref.theme + theme(legend.position = c(0.85, 0.7)) +
+  theme(axis.title.x = element_blank()) + no.y.axis + pref.panel.bord; sdmx.abun.plt
 
   ## Make a two-panel figure
 sdmx.abun.fig <- egg::ggarrange(sdmx.14.abun.plt, sdmx.abun.plt, nrow = 1, widths = c(1, 2.5))
@@ -352,8 +359,8 @@ sdmx.14.rich.plt <- ggplot(sdmx.14.rich.pltdf, aes(x = Herb.Trt, y = Richness)) 
   scale_color_manual(values = sdmx.colors) +
   scale_fill_manual(values = sdmx.colors) +
   ylim(0, 8.5) +
-  labs(x = "Pre-Treatment", y = "Seeded Species Richness") +
-  pref.theme; sdmx.14.rich.plt
+  labs(x = "Pre-treatment", y = "Seeded species richness") +
+  pref.theme + pref.panel.bord; sdmx.14.rich.plt
 
 ## Post-Treatment (2015-18)
 sdmx.rich.plt <- ggplot(sdmx.rich.pltdf, aes(x = Year, y = Richness)) +
@@ -361,21 +368,18 @@ sdmx.rich.plt <- ggplot(sdmx.rich.pltdf, aes(x = Year, y = Richness)) +
   geom_errorbar(aes(ymax = Richness + se, ymin = Richness - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 8.5) +
-  geom_text(label = "A", x = 15.5, y = 12) +
-  geom_text(label = "AB", x = 14.85, y = 14.5) +
-  geom_text(label = "B", x = 15.3, y = 18) +
   scale_fill_manual(values = sdmx.colors) +
   scale_color_manual(values = sdmx.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + no.y.axis; sdmx.rich.plt
+  pref.theme + no.y.axis + pref.panel.bord; sdmx.rich.plt
 
   ## Make a two-panel figure
 sdmx.rich.fig <- egg::ggarrange(sdmx.14.rich.plt, sdmx.rich.plt, nrow = 1, widths = c(1, 2.5))
 
 # Make two panel seedmix figure
-plot_grid(sdmx.abun.fig, sdmx.rich.fig, labels = c("i", "ii"), ncol = 1, nrow = 2)
+plot_grid(sdmx.abun.fig, sdmx.rich.fig, labels = c("A", "B"), align = 'v', ncol = 1, nrow = 2)
 
 # Save it
 ggsave(plot = last_plot(), filename = "./Figures/Figure 4.pdf", width = 7, height = 9, units = "in")
@@ -416,8 +420,8 @@ nv.ex.14.plt <- ggplot(nv.ex.14.pltdf, aes(x = Herb.Trt, y = Percent.Native)) +
   scale_color_manual(values = sdmx.colors) +
   scale_fill_manual(values = sdmx.colors) +
   ylim(0, 70) +
-  labs(x = "Pre-Treatment", y = "Percent Native Flowers") +
-  pref.theme; nv.ex.14.plt
+  labs(x = "Pre-treatment", y = "Percent native flowers") +
+  pref.theme + pref.panel.bord; nv.ex.14.plt
 
 ## Post-Treatment (2015-18)
 nv.ex.plt <- ggplot(nv.ex.pltdf, aes(x = Year, y = Percent.Native)) +
@@ -425,12 +429,12 @@ nv.ex.plt <- ggplot(nv.ex.pltdf, aes(x = Year, y = Percent.Native)) +
   geom_errorbar(aes(ymax = Percent.Native + se, ymin = Percent.Native - se,
                     color = Herb.Trt), width = 0.45, position = dodge) +
   geom_point(aes(fill = Herb.Trt, shape = Herb.Trt), size = 2, position = dodge) +
-  labs(x = "Post-Treatment") +
+  labs(x = "Post-treatment") +
   ylim(0, 70) +
   scale_fill_manual(values = sdmx.colors) +
   scale_color_manual(values = sdmx.colors) +
   scale_shape_manual(values = 22:24) +
-  pref.theme + theme(legend.position = c(0.85, 0.95)) + no.y.axis; nv.ex.plt
+  pref.theme + theme(legend.position = c(0.3, 0.825)) + no.y.axis + pref.panel.bord; nv.ex.plt
 
 ## Make a two-panel figure
 nv.ex.fig <- egg::ggarrange(nv.ex.14.plt, nv.ex.plt, nrow = 1, widths = c(1, 2.5))
